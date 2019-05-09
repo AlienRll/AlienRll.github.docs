29feb3e11316b73669688a3d15519e4f8e67a64b
alienrll <348744588@qq.com>
2019-05-09 23:20:11 +0800

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'Hello E',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: 'http://baidu.com' },
+            { text: 'Guide', link: 'https://www.jianshu.com' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            // '/',
+            // '/page-a',
+            // ['/page-b', 'Explicit link text']
+        ]
+    }
+}
